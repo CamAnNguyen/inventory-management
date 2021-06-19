f6a4e159012ca84cd5c67602c010d222f5545a22
An Nguyen <caman.nguyenthanh@gmail.com>
2021-06-19 09:22:45 +0700

    Rspec for FindFulfillableOrder in race condition

--- spec/services/find_fulfillable_order_spec.rb
+++ spec/services/find_fulfillable_order_spec.rb
@@ -21,4 +21,40 @@ RSpec.describe FindFulfillableOrder do
 
     expect(FindFulfillableOrder.begin_fulfillment(employee)).to be_nil
   end
+
+  it 'fulfills properly parallely' do
+    num_threads = 4
+    quantity = 3
+
+    employees = []
+    orders = []
+    product = create(:product)
+
+    num_threads.times do
+      employee = create(:employee)
+      employees.push(employee)
+
+      order = create(
+        :order,
+        line_items: [build(:order_line_item, product: product, quantity: 1)]
+      )
+      orders.push(order)
+    end
+
+    quantity.times do |i|
+      ReceiveProduct.run(employees[i], product, 1)
+    end
+
+    product.reload
+    expect(product.on_shelf).to eq(quantity)
+
+    num_threads.times.map do |i|
+      Thread.new do
+        FindFulfillableOrder.fulfill_order(employees[i], orders[i].id)
+      end
+    end.each(&:join)
+
+    # Even with 4 threads, only 3 inventories are created
+    expect(Inventory.count).to eq(quantity)
+  end
 end
